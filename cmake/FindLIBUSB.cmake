9e1f77d9ccfe713b427e1a2842d25a16420cc257
pieniacy <karol.pieniacy@gmail.com>
2021-03-16 13:31:14 +0100

    Update CMakeLists.txt to use PROJECT_SOURCE_DIR
    Change CMAKE_HOME_DIRECTORY and CMAKE_SOURCE_DIR varaibles to PROJECT_SOURCE_DIR.
    This should not change anything when compiling this repo as a standalone build.
    This allows embedding library sources along with cmake build infrastructure in external projects simply with cmake ADD_SUBDIRECTORY command.

--- cmake/FindLIBUSB.cmake
+++ cmake/FindLIBUSB.cmake
@@ -132,7 +132,7 @@ if ( LibUSB_FOUND )
 
         try_compile(LibUSB_COMPILE_TEST_PASSED
                 ${CMAKE_BINARY_DIR}
-                "${CMAKE_SOURCE_DIR}/cmake/test-libusb-version.cpp"
+                "${PROJECT_SOURCE_DIR}/cmake/test-libusb-version.cpp"
                 CMAKE_FLAGS
                     "-DINCLUDE_DIRECTORIES=${LibUSB_INCLUDE_DIRS}"
                     "-DLINK_DIRECTORIES=${LibUSB_LIBRARIES}"
