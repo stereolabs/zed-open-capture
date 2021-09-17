fe6dea652c06b057d70f70b94f30b861d49829e6
Myzhar <Myzhar@users.noreply.github.com>
2021-09-17 15:41:49 +0000

    deploy: 50cb6dcfa78fc92bbe40f00aa12b5e798ac4fe8e

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['mag_5fvalid_687',['mag_valid',['../db/dd1/structsl__oc_1_1sensors_1_1usb_1_1_raw_data.html#af02a1836ac65384be9c5e1968492a81a',1,'sl_oc::sensors::usb::RawData']]],
-  ['maxdepth_5fmm_688',['maxDepth_mm',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#aebc8ccac34204d3ce0bf1ea4cf0c23d8',1,'sl_oc::tools::StereoSgbmPar']]],
+  ['maxdepth_5fmm_688',['maxDepth_mm',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#ac7583f2a31dad1def370a0c6b66f3a84',1,'sl_oc::tools::StereoSgbmPar']]],
   ['maxmaxdisp_689',['maxMaxDisp',['../dc/d2e/zed__oc__tune__stereo__sgbm_8cpp.html#a91e793a2e74eeb6def57268834ae5317',1,'zed_oc_tune_stereo_sgbm.cpp']]],
   ['mcu_5fsync_5fts_690',['mcu_sync_ts',['../da/d5d/zed__oc__sync__example_8cpp.html#a38cf151a5c361c0ed7d3df6ef162cb05',1,'zed_oc_sync_example.cpp']]],
-  ['mindepth_5fmm_691',['minDepth_mm',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#a574cb2d80b00884c71bb01449f329d45',1,'sl_oc::tools::StereoSgbmPar']]],
+  ['mindepth_5fmm_691',['minDepth_mm',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#a46c3af7e5fe0625cddc3dcf7a3315dc8',1,'sl_oc::tools::StereoSgbmPar']]],
   ['mindisparity_692',['minDisparity',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#a47e39722f042eb7c1498fca47084a708',1,'sl_oc::tools::StereoSgbmPar']]],
   ['mode_693',['mode',['../d9/de8/classsl__oc_1_1tools_1_1_stereo_sgbm_par.html#a8f2720be5b8bfbda04bf5940c5ac6512',1,'sl_oc::tools::StereoSgbmPar']]],
   ['mx_694',['mX',['../da/d29/structsl__oc_1_1sensors_1_1data_1_1_magnetometer.html#a6f7e3eab66b9a65b64045accf0d78328',1,'sl_oc::sensors::data::Magnetometer::mX()'],['../db/dd1/structsl__oc_1_1sensors_1_1usb_1_1_raw_data.html#aabb1fbcd925d08b849f08c462e48ba6a',1,'sl_oc::sensors::usb::RawData::mX()']]],
